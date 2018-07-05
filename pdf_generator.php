0452f517bc3645f07b8b97de5e805750d123f662
ilpise <pisewip@yahoo.it>
2018-07-05 18:10:00 +0200

    Added get parameter (?code=20161164TF911) for label highlight

--- pdf_generator.php
+++ pdf_generator.php
@@ -1,14 +1,21 @@
-
+<!DOCTYPE html>
+<html>
+<head>
 <script type="text/javascript" src="./jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="./snap.svg.js"></script>
+<!-- <script type="text/javascript" src="./simple.js"></script> --><!-- NON funziona -->
+</head>
 
-
-
-<div class="my-svg-container">
+<body>
 	<div class="my-svg-container">
-		<svg id="svg_div" class="my-svg" preserveAspectRatio= "xMinYMin meet" viewBox="0 0 800 960" ></svg>
+		<div class="my-svg-container">
+			<svg id="svg_div" class="my-svg" preserveAspectRatio= "xMinYMin meet" viewBox="0 0 800 960" ></svg>
+		</div>
 	</div>
-</div>
+	<!-- <div>
+		<img src="./svg/20161160FA024.svg" />
+	</div> -->
+	<script type="text/javascript" src="./svgprintpdf.js"></script>
 
-
-<script type="text/javascript" src="./svgprintpdf.js"></script>
+</body>
+</html>
